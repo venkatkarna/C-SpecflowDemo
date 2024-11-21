c3eb44fd1ebe8c9fa4a639196420fae805b6b496
venkatkarna <venkateshbaskar49@gmail.com>
2024-11-21 12:13:07 +0530

    files added

---
+++ Features/Sample.feature
@@ -0,0 +1,9 @@
+﻿Feature: Sample
+
+A short summary of the feature
+
+@tag1
+Scenario: Search for TestersTalk 
+	Given open the browser
+	When Enter the URL
+	Then Search for TestersTalk
---
+++ StepDefinitions/Sample.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlowProjectDemo.StepDefinitions
+{
+
+    [Binding]
+    public class Sample
+    {
+        private IWebDriver driver;
+
+        [Given(@"open the browser")]
+        public void GivenOpenTheBrowser()
+        {
+            driver = new ChromeDriver();
+
+            driver.Manage().Window.Maximize();
+
+        }
+
+        [When(@"Enter the URL")]
+        public void WhenEnterTheURL()
+        {
+            driver.Url = "https://www.youtube.com/";
+            Thread.Sleep(5000);
+        }
+
+        [Then(@"Search for TestersTalk")]
+        public void ThenSearchForTestersTalk()
+        {
+            driver.FindElement(By.XPath("//input[@id='search']")).SendKeys("TestersTalk");
+            driver.FindElement(By.XPath("//input[@id='search']")).SendKeys(Keys.Enter);
+            Thread.Sleep(5000);
+            driver.Quit();
+
+        }
+
+    }
+}
