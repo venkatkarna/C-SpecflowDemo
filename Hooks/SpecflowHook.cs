7b3e49d283fab0efe1d5a751d45bc0d254626f2f
venkatkarna <venkateshbaskar49@gmail.com>
2024-11-21 13:20:51 +0530

    fooks added

---
+++ Hooks/SpecflowHook.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlowProjectDemo.Hooks
+{
+  
+[Binding]
+    public class SpecFlowHooks
+    {
+        private IWebDriver driver;
+
+        // Initialize WebDriver before the scenario
+        [BeforeScenario]
+        public void InitializeWebDriver()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--headless");        
+            options.AddArgument("--disable-gpu");    
+            options.AddArgument("--no-sandbox");     
+
+            driver = new ChromeDriver(options);
+        }
+
+        [AfterScenario]
+        public void CloseWebDriver()
+        {
+            driver.Quit();
+        }
+
+        // Property to access the driver in your step definitions
+        public IWebDriver Driver => driver;
+    }
+
+}
+
